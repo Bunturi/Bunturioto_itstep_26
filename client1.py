59f03146068f6a1988d57b6f8e70bd9de56996e3
bunturioto <Bunturioto@gmail.com>
2024-03-31 12:27:11 +0400

    Create a socket + connection + Receive and prin for client1.py

--- client1.py
+++ client1.py
@@ -3,3 +3,10 @@ import socket
 # Server configuration
 HOST = '127.0.0.1'
 PORT = 65432
+
+# Create a socket + connection + Receive and print
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+received_message = client_socket.recv(1024).decode()
+print(received_message)
+client_socket.close()

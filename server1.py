ea3aee53430f93729134ab0dd9a5e771023cdc34
bunturioto <Bunturioto@gmail.com>
2024-03-31 12:24:23 +0400

    Accept incoming connection

--- server1.py
+++ server1.py
@@ -9,3 +9,12 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen()
 print(f"Waiting for connections on {HOST}:{PORT}")
+
+while True:
+    # Accept incoming connection
+    conn, addr = server_socket.accept()
+
+    print('Connected by', addr)
+    message = "server accepted Connection"
+    conn.sendall(message.encode())
+    conn.close()

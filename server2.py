f96481e2af35f127209d1fde2b13d33373ebf203
bunturioto <Bunturioto@gmail.com>
2024-03-31 12:30:28 +0400

    Server configuration+Create a socket and start listening

---
+++ server2.py
@@ -0,0 +1,13 @@
+import socket
+
+# Server configuration
+HOST = '127.0.0.1'
+PORT = 65432
+
+# Create a socket and start listening
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
+
+server_socket.bind((HOST, PORT))
+server_socket.listen()
+print(f"Start listening on {PORT}")

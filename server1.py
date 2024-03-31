9d5cec1ee3302fc484ed809c496d7feeff35c9be
bunturioto <Bunturioto@gmail.com>
2024-03-31 12:22:57 +0400

    Create a socket + Bind + Start listening

--- server1.py
+++ server1.py
@@ -3,3 +3,9 @@ import socket
 # Server configuration
 HOST = '127.0.0.1'
 PORT = 65432
+
+# Create a socket + Bind + Start listening
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
+print(f"Waiting for connections on {HOST}:{PORT}")

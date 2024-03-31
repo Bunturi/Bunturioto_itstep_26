4f4443b46554fd1e6267c5a0762a872a9316d70f
bunturioto <Bunturioto@gmail.com>
2024-03-31 12:49:45 +0400

    Receives messages from the server.If the message is Nickname, sends the client's nickname to the server. Otherwise, prints the received message.

--- client2.py
+++ client2.py
@@ -10,3 +10,18 @@ nickname = input("Enter your nickname: ")
 # Create a socket object for the client + Connect
 client_socket = socket.socket()
 client_socket.connect((SERVER_IP, SERVER_PORT))
+
+
+# Receives messages from the server
+def receive():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+            if message == "Nickname":
+                client_socket.send(nickname.encode())
+            else:
+                print(message)
+        except Exception as e:
+            print("Error", e)
+            client_socket.close()
+            break
